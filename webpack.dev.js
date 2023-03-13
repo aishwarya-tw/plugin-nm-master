079e6dd241508ac2147c9d9fefdfd2bb1128879c
Aishwarya Bannimatti <abannimatti@twilio.com>
2023-03-13 17:14:21 +0530

    Removed webpack config for dev server as it is not required

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,16 +4,5 @@ module.exports = (config, { isProd, isDev, isTest }) => {
    * Consult https://webpack.js.org/configuration/dev-server for more information.
    */
 
-  config.module = {
-    rules: [
-      {
-        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: "file-loader?name=assets/[name].[hash].[ext]",
-      },
-      {
-        test: /\.mjs$/,
-        type: "javascript/auto",
-      },
-    ],
-  };
+  return config;
 };
